9e54aa7b651b3951557f7fa2ac5dcf9054a0db58
ClinicalShelter <49001314+ClinicalShelter@users.noreply.github.com>
2021-07-13 02:12:58 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+package Bank;
+import java.util.*;
+
+public class Main {
+/*	public static double compound_calculator(double interest, double money, double time) {
+		double total = money * (Math.pow(interest + 1, time));
+		return total;
+	}
+	public static void something() {
+		String[] choices = {"[1] Check balance","[2] Deposit money", "[3] Withdrawal money", "[4] Intrest" ,"[5] Exit"};
+		String name = "Bob";
+		double amount_in_bank_account = 5000;
+		double interest_rate_savings = 0.0185;
+		double interest_rate_loan = 0.140;
+		double new_debt = 0;
+		int length_of_time_for_10_years = 10;
+		int length_of_time_for_5_years = 5;
+		int x = 0;
+		double withdarawl = 0;
+		double amount_deposit = 0;
+		boolean exit = false;
+		String good_bye = "Have a great day!";
+		String bank_name = "Well's fargo";
+		String First_thing_the_bank_will_say = "Welcome to " + bank_name + "! Glad you are back " + name + "!";
+		choices();
+	}
+	public static void choices() {
+		
+	} */
+	public static void main(String[] args) {
+		// something();
+		System.out.println("TEST");
+	}
+}
